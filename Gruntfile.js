db911c7d00f081bdb3d3ac5a30d9080765b434cf
Svetoslav Denev <svetoslav.denev@lecturio.de>
2018-03-17 13:38:18 +0100

    Added React Native target for Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,107 +1,123 @@
 module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-    grunt.initConfig({
+    banner:
+      '/**\n' +
+      ' * Copyright 2014 Amazon.com,\n' +
+      ' * Inc. or its affiliates. All Rights Reserved.\n' +
+      ' * \n' +
+      ' * Licensed under the Amazon Software License (the "License").\n' +
+      ' * You may not use this file except in compliance with the\n' +
+      ' * License. A copy of the License is located at\n' +
+      ' * \n' +
+      ' *     http://aws.amazon.com/asl/\n' +
+      ' * \n' +
+      ' * or in the "license" file accompanying this file. This file is\n' +
+      ' * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR\n' +
+      ' * CONDITIONS OF ANY KIND, express or implied. See the License\n' +
+      ' * for the specific language governing permissions and\n' +
+      ' * limitations under the License. \n' +
+      ' */\n\n',
 
-        pkg: grunt.file.readJSON('package.json'),
+    qunit: {
+      all: ['tst/**/*.html'],
+    },
 
-        banner: '/**\n' +
-        ' * Copyright 2014 Amazon.com,\n' +
-        ' * Inc. or its affiliates. All Rights Reserved.\n' +
-        ' * \n' +
-        ' * Licensed under the Amazon Software License (the "License").\n' +
-        ' * You may not use this file except in compliance with the\n' +
-        ' * License. A copy of the License is located at\n' +
-        ' * \n' +
-        ' *     http://aws.amazon.com/asl/\n' +
-        ' * \n' +
-        ' * or in the "license" file accompanying this file. This file is\n' +
-        ' * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR\n' +
-        ' * CONDITIONS OF ANY KIND, express or implied. See the License\n' +
-        ' * for the specific language governing permissions and\n' +
-        ' * limitations under the License. \n' +
-        ' */\n\n',
-
-        qunit: {
-            all: ['tst/**/*.html']
+    jshint: {
+      options: {
+        reporterOutput: '',
+        browser: true,
+        eqeqeq: true,
+        globals: {
+          AWS: true,
         },
+        undef: true,
+      },
 
-        jshint: {
+      src: ['src/*.js'],
+    },
 
-            options: {
-                reporterOutput: '',
-                browser: true,
-                eqeqeq: true,
-                globals: {
-                    AWS: true
-                },
-                undef: true
-            },
-
-            src: ['src/*.js']
-
+    jsdoc: {
+      dist: {
+        src: ['src/*.js'],
+        options: {
+          destination: 'docs',
         },
+      },
+    },
 
-        jsdoc: {
-            dist: {
-                src: ['src/*.js'],
-                options: {
-                    destination: 'docs'
-                }
-            }
+    uglify: {
+      options: {
+        sourceMap: true,
+        drop_console: true,
+        banner: '<%= banner %>',
+      },
+      dist: {
+        files: {
+          'dist/amazon-cognito.min.js': [
+            'src/CognitoSyncManager.js',
+            'src/CognitoSyncConflict.js',
+            'src/CognitoSyncDataset.js',
+            'src/CognitoSyncDatasetMetadata.js',
+            'src/CognitoSyncDatasetUpdates.js',
+            'src/CognitoSyncLocalStorage.js',
+            'src/CognitoSyncRecord.js',
+            'src/CognitoSyncRemoteStorage.js',
+            'src/CognitoSyncStoreInMemory.js',
+            'src/CognitoSyncStoreLocalStorage.js',
+          ],
+          'dist/amazon-cognito-rn.min.js': [
+            'src/CognitoSyncManager.js',
+            'src/CognitoSyncConflict.js',
+            'src/CognitoSyncDataset.js',
+            'src/CognitoSyncDatasetMetadata.js',
+            'src/CognitoSyncDatasetUpdates.js',
+            'src/CognitoSyncLocalStorage.js',
+            'src/CognitoSyncRecord.js',
+            'src/CognitoSyncRemoteStorage.js',
+            'src/CognitoSyncStoreInMemory.js',
+            'src/CognitoSyncStoreLocalStorage.js',
+          ],
         },
+      },
+    },
 
-        uglify: {
-            options: {
-                sourceMap: true,
-                drop_console: true,
-                banner: '<%= banner %>'
-            },
-            dist: {
-                files: {
-                    "dist/amazon-cognito.min.js": [
-                        'src/CognitoSyncManager.js',
-                        'src/CognitoSyncConflict.js',
-                        'src/CognitoSyncDataset.js',
-                        'src/CognitoSyncDatasetMetadata.js',
-                        'src/CognitoSyncDatasetUpdates.js',
-                        'src/CognitoSyncLocalStorage.js',
-                        'src/CognitoSyncRecord.js',
-                        'src/CognitoSyncRemoteStorage.js',
-                        'src/CognitoSyncStoreInMemory.js',
-                        'src/CognitoSyncStoreLocalStorage.js'
-                    ]
-                }
-            }
+    umd: {
+      all: {
+        options: {
+          src: 'dist/amazon-cognito.min.js',
+          deps: {
+            default: [{ 'aws-sdk/global': 'AWS' }],
+            global: [{ AWS: 'AWS' }],
+          },
         },
-
-        umd: {
-            all: {
-                options: {
-                    src: 'dist/amazon-cognito.min.js',
-                    deps: {
-                        default: [{'aws-sdk/global': 'AWS'}],
-                        global: [{'AWS': 'AWS'}]
-                    }
-                }
-            }
+      },
+      rn: {
+        options: {
+          src: 'dist/amazon-cognito-rn.min.js',
+          deps: {
+            default: [{ 'aws-sdk/dist/aws-sdk-react-native': 'AWS' }],
+            global: [{ AWS: 'AWS' }],
+          },
         },
+      },
+    },
 
-        watch: {
-            scripts: {
-                files: ['src/*.js'],
-                tasks: ['uglify:dist']
-            }
-        }
+    watch: {
+      scripts: {
+        files: ['src/*.js'],
+        tasks: ['uglify:dist'],
+      },
+    },
+  });
 
-    });
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-jsdoc');
+  grunt.loadNpmTasks('grunt-umd');
 
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-qunit');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-jsdoc');
-    grunt.loadNpmTasks('grunt-umd');
-
-    grunt.registerTask('default', ['qunit']);
-
+  grunt.registerTask('default', ['qunit']);
 };
